eb5d2f2ccccef3a168fc0f427cd303cb8db0f19b
Jonathan Mayer <jmayer@stanford.edu>
2011-09-01 16:36:07 -0700

    moving all modal disables from crawling script to modal-disable extension

--- crawling/modal-disable/lib/main.js
+++ crawling/modal-disable/lib/main.js
@@ -1,7 +1,30 @@
+var prefs = require("preferences-service");
 var pageMod = require("page-mod");
 const data = require("self").data;
 
 exports.main = function(options, callbacks) {
+	
+	// Disable security warnings
+	var securityWarningRoot = "security.warn_";
+	var securityWarningDisableList = ["entering_secure", "entering_weak", "leaving_secure", "submit_insecure", "viewing_mixed"];
+	for (var i = 0; i < securityWarningDisableList.length; i++) {
+		prefs.set(securityWarningRoot + securityWarningDisableList[i], false);
+		prefs.set(securityWarningRoot + securityWarningDisableList[i] + ".show_once", false);
+	}
+	
+	// Use configurable security policies to disable window.* modals
+	// Disabled for now because content script approach doesn't throw security errors
+	/*
+	var cspWindowRoot = "capability.policy.default.Window.";
+	var cspWindowDisableList = ["open", "alert", "confirm", "prompt", "onbeforeunload", "onunload"];
+	for (var i = 0; i < cspWindowDisableList.length; i++)
+		prefs.set(cspWindowRoot + cspWindowDisableList[i], "noAccess");
+	*/
+	
+	// Bump the maximum script run time to prevent "unresponsive script" errors
+	prefs.set("dom.max_script_run_time", 60);
+	
+	// Inject content script to disable window.* modals
 	pageMod.PageMod({
 		include: "*",
 		contentScriptWhen: "start",
--- crawling/mozmill_crawl_template.js
+++ crawling/mozmill_crawl_template.js
@@ -13,16 +13,8 @@ var MOZMILL_TEST_ROOT = "";
 var PrivateBrowsingAPI = require(MOZMILL_TEST_ROOT + "lib/private-browsing");
 var privateBrowsing;
 
-var PrefsAPI = require(MOZMILL_TEST_ROOT + "lib/prefs");
-var preferences = PrefsAPI.preferences;
-var cspWindowRoot = "capability.policy.default.Window.";
-var cspWindowDisableList = ["open", "alert", "confirm", "prompt", "onbeforeunload", "onunload"];
-var securityWarningRoot = "security.warn_";
-var securityWarningDisableList = ["entering_secure", "entering_weak", "leaving_secure", "submit_insecure", "viewing_mixed"];
-
 var PAGE_LOAD_TIMEOUT = 10000;
 var PAGE_WAIT = 10000;
-var MAX_SCRIPT_RUN_TIME = 30;
 
 var setupModule = function(module) {
 	module.controller = mozmill.getBrowserController();
@@ -37,15 +29,6 @@ var setupModule = function(module) {
 		privateBrowsing.start();
 		privateBrowsing.waitForTransitionComplete(true);
 	};
-	
-	// Setup configurable security policies
-	for (var i = 0; i < cspWindowDisableList.length; i++)
-		preferences.prefBranch.setCharPref(cspWindowRoot + cspWindowDisableList[i], "noAccess");
-	// Disable security warnings
-	for (var i = 0; i < securityWarningDisableList.length; i++)
-		preferences.prefBranch.setBoolPref(securityWarningRoot + securityWarningDisableList[i], false);
-	// Bump the maximum script run time to prevent "unresponsive script" errors
-	preferences.prefBranch.setIntPref("dom.max_script_run_time", MAX_SCRIPT_RUN_TIME);
 }
 // BEGIN_REPEAT
 
@@ -59,9 +42,4 @@ var testURL_NUMBER = function(){
 
 var teardownModule = function(module) {
 	privateBrowsing.stop();
-	for (var i = 0; i < cspWindowDisableList.length; i++)
-		preferences.prefBranch.clearUserPref(cspWindowRoot + cspWindowDisableList[i]);
-	for (var i = 0; i < securityWarningDisableList.length; i++)
-		preferences.prefBranch.clearUserPref(securityWarningRoot + securityWarningDisableList[i]);
-	preferences.prefBranch.clearUserPref("dom.max_script_run_time");
 }
