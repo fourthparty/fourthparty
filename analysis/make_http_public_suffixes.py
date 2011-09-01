ca9e2f8c465ae98eb3cd05b749133f565f5de5ed
Jonathan Mayer <jmayer@stanford.edu>
2011-08-31 17:30:56 -0700

    updating make_public_suffixes to ignore IPv4 addresses

--- analysis/make_http_public_suffixes.py
+++ analysis/make_http_public_suffixes.py
@@ -3,6 +3,7 @@
 import sys
 import os.path
 import sqlite3
+import re
 import urlparse
 import publicsuffix
 
@@ -15,6 +16,7 @@ dbConnection = sqlite3.connect(dbFileName)
 dbConnection.row_factory = sqlite3.Row
 dbCursor = dbConnection.cursor()
 
+ipRegex = re.compile(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}")
 psl = publicsuffix.PublicSuffixList()
 
 dbCursor.execute("ALTER TABLE http_requests ADD public_suffix TEXT")
@@ -24,7 +26,10 @@ for httpRequestRow in httpRequestRows:
 	httpRequestId = httpRequestRow['id']
 	httpRequestURL = httpRequestRow['url']
 	httpRequestHostName = urlparse.urlparse(httpRequestURL).hostname
-	httpRequestPublicSuffix = psl.get_public_suffix(httpRequestHostName)
+	if ipRegex.match(httpRequestHostName):
+		httpRequestPublicSuffix = httpRequestHostName
+	else:
+		httpRequestPublicSuffix = psl.get_public_suffix(httpRequestHostName)
 	dbCursor.execute("UPDATE http_requests SET public_suffix=? WHERE id=?", (httpRequestPublicSuffix, httpRequestId))
 
 dbCursor.execute("ALTER TABLE http_responses ADD public_suffix TEXT")
@@ -34,7 +39,10 @@ for httpResponseRow in httpResponseRows:
 	httpResponseId = httpResponseRow['id']
 	httpResponseURL = httpResponseRow['url']
 	httpResponseHostName = urlparse.urlparse(httpResponseURL).hostname
-	httpResponsePublicSuffix = psl.get_public_suffix(httpResponseHostName)
+	if ipRegex.match(httpResponseHostName):
+		httpRequestPublicSuffix = httpResponseHostName
+	else:
+		httpResponsePublicSuffix = psl.get_public_suffix(httpResponseHostName)
 	dbCursor.execute("UPDATE http_responses SET public_suffix=? WHERE id=?", (httpResponsePublicSuffix, httpResponseId))
 
 dbConnection.commit()
