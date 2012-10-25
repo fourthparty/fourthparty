11d51220fec2bfa9d1249d06736af8fe9ddb53ec
Jonathan Mayer <jmayer@stanford.edu>
2012-10-24 17:49:56 -0700

    adding multi-tabbed crawling script

---
+++ crawling/mozmill_crawl_template_multitab.js
@@ -0,0 +1,80 @@
+// Point to a local copy of the latest mozmill-test repository
+// See https://developer.mozilla.org/en/Mozmill_Tests/Shared_Modules
+// On Windows, format path like file://C:/...
+var MOZMILL_TEST_ROOT = "file://C:/Users/Administrator/FourthParty/mozmill-tests-4db9ba8beeaf/mozmill-tests-4db9ba8beeaf/";
+
+var PrivateBrowsingAPI = require(MOZMILL_TEST_ROOT + "lib/private-browsing");
+var privateBrowsing;
+
+var PAGE_WAIT = 15000;
+
+var TABS = 5;
+var availableTabs = [ ];
+var loadingTime = [ ];
+
+var setupModule = function(module) {
+	module.controller = mozmill.getBrowserController();
+	
+	// Setup private browsing control
+
+	privateBrowsing = new PrivateBrowsingAPI.privateBrowsing(module.controller);
+	privateBrowsing.showPrompt = false;
+	privateBrowsing.waitForTransitionComplete = privateBrowsing.waitForTransistionComplete;
+	module.cyclePrivateBrowsing = function() {
+		privateBrowsing.stop();
+		privateBrowsing.waitForTransitionComplete(false);
+		privateBrowsing.start();
+		privateBrowsing.waitForTransitionComplete(true);
+		for(var i = 0; i < TABS - 1; i++)
+			controller.click(new elementslib.Elem(controller.menus['file-menu']['menu_newNavigatorTab']));
+		availableTabs = [ ];
+		loadingTime = [ ];
+		for(var i = 0; i < TABS; i++) {
+			availableTabs.push(i);
+			loadingTime.push(-1);
+		}
+	};
+	
+	module.clearLoadedTabs = function() {
+		for(var i = 0; i < TABS; i++)
+			if(loadingTime[i] > 0 && (new Date()).getTime() - loadingTime[i] > PAGE_WAIT) {
+				loadingTime[i] = -1;
+				availableTabs.push(i);
+			}
+	};
+	
+	module.hasFreeTab = function() {
+		if(availableTabs.length > 0)
+			return true;
+		return false;
+	};
+	
+	module.waitForFreeTab = function() {
+		controller.waitFor(function() {
+			clearLoadedTabs();
+			if(hasFreeTab())
+				return true;
+			return false;
+		}, "", 24*60*60*1000, 100, this);
+	};
+}
+// BEGIN_REPEAT
+
+var testURL_NUMBER = function(){
+	var nextPageLocation = "URL";
+	if(nextPageLocation == "about:blank")
+		cyclePrivateBrowsing();
+
+	waitForFreeTab();
+	
+	var tab = availableTabs[0];
+	availableTabs.shift();
+	controller.tabs.selectTabIndex(tab);
+	loadingTime[tab] = (new Date()).getTime();
+	controller.open(nextPageLocation);
+}
+// END_REPEAT
+
+var teardownModule = function(module) {
+	privateBrowsing.stop();
+}
