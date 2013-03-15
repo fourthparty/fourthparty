c3e4a2472f12523e859105a57cb3aaba350f9b2f
Felix <felix.shnir@gmail.com>
2013-03-15 12:04:51 -0400

    New content policy registration

--- extension/lib/content-policy-instrument.js
+++ extension/lib/content-policy-instrument.js
@@ -1,6 +1,7 @@
 const Cc = require('chrome').Cc;
 const Ci = require('chrome').Ci;
 const Cu = require('chrome').Cu;
+const Cm = require('chrome').Cm;
 const components = require('chrome').components;
 
 const data = require("self").data;
@@ -17,19 +18,29 @@ exports.run = function() {
 
 	// Instrument content policy API
 	// Provides additional information about what caused a request and what it's for
-	function InstrumentContentPolicy() {}
-	InstrumentContentPolicy.prototype = {
+	var InstrumentContentPolicy = {
+		QueryInterface: xpcomUtils.generateQI([Ci.nsIContentPolicy]),
+		classID: require('sdk/util/uuid').uuid(),
 		classDescription: "Instruments the content policy API",
 		contractID: "@stanford.edu/instrument-content-policy;1",
-		classID: require('sdk/util/uuid').uuid(),
-		QueryInterface: xpcomUtils.generateQI([Ci.nsIContentPolicy]),
-		
+
+		register: function() {
+			var registrar = Cm.QueryInterface(Ci.nsIComponentRegistrar),
+			  catMan = Cc['@mozilla.org/categorymanager;1'].getService(Ci.nsICategoryManager);
+				
+			registrar.registerFactory(this.classID, this.classDescription, this.contractID, this);
+			catMan.addCategoryEntry('content-policy', this.contractID, this.contractID, false, true);
+
+			this.factoryAddonManagement();			
+		},
+
 		shouldLoad: function(contentType, contentLocation, requestOrigin, context, mimeTypeGuess, extra) {
 			var update = { };
 			update["content_type"] = contentType;
 			update["content_location"] = loggingDB.escapeString(contentLocation.spec);
 			update["request_origin"] = loggingDB.escapeString(requestOrigin ? requestOrigin.spec : "");
 			update["page_id"] = -1;
+
 			if(context) {
 				var domNode = null;
 				var domWindow = null;
@@ -57,28 +68,46 @@ exports.run = function() {
 		// Fires infrequently, instrumentation unused
 		shouldProcess: function(contentType, contentLocation, requestOrigin, context, mimeType, extra) {
 			return Ci.nsIContentPolicy.ACCEPT;
+		},
+
+		// nsIFactory interface implementation
+		createInstance: function(outer, iid) {
+			if (outer) { throw Cr.NS_ERROR_NO_AGGREGATION; }
+			return this.QueryInterface(iid);
+		},
+
+		/**
+		 * Looks like Addon SDK forgets to remove previous versions content policy.
+		 */
+		factoryAddonManagement: function () {
+			try {
+				Cu.import('resource://gre/modules/AddonManager.jsm');
+				AddonManager.addAddonListener({
+					onUninstalling: function(addon) {
+						InstrumentContentPolicy.factoryUnregister(addon);
+					},
+					onInstalling: function (addon) {
+						InstrumentContentPolicy.factoryUnregister(addon);
+					}
+				});
+			} catch (e) {}
+		},
+	
+		factoryUnregister: function (addon) {
+			// Checks if factory already exists
+			if (addon.id == 'fourthparty@fourthparty.info') {
+				try {
+					var registrar = Cm.QueryInterface(Ci.nsIComponentRegistrar),
+						catMan = Cc['@mozilla.org/categorymanager;1'].getService(Ci.nsICategoryManager);
+	
+					catMan.deleteCategoryEntry('content-policy', InstrumentContentPolicy.contractID, InstrumentContentPolicy.BlockingPolicy);
+	
+					registrar.unregisterFactory(InstrumentContentPolicy.classID, InstrumentContentPolicy);
+				} catch (e) {}
+			}
 		}
 	};
 
-var xpcom = require('sdk/platform/xpcom');
- 
-var factory = xpcom.Factory({
-  contract: InstrumentContentPolicy.prototype.contractID,
-  Component: InstrumentContentPolicy,
-  register: false,
-  unregister: false,
-});
-
-xpcom.register(factory);
-/*
-	xpcom.register({
-		create: InstrumentContentPolicy,
-		name: InstrumentContentPolicy.prototype.classDescription,
-		contractID: InstrumentContentPolicy.prototype.contractID,
-		uuid: InstrumentContentPolicy.prototype.classID
-	});
-*/
-	var categoryManager = Cc["@mozilla.org/categorymanager;1"].getService(Ci.nsICategoryManager);
-	categoryManager.addCategoryEntry("content-policy", InstrumentContentPolicy.prototype.contractID, InstrumentContentPolicy.prototype.contractID, false, false);
-	
+	// XPCOM registration
+	InstrumentContentPolicy.register();
 };
